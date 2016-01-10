01b9e30d0d76542109a43e65d6eb848987e6ece5
Thomas Gardner <tpgardner19@gmail.com>
2016-01-09 21:43:57 -0600

    added extra comments on debug symbols and the Exception constructor

--- myexception.d
+++ myexception.d
@@ -1,5 +1,9 @@
+// Exceptions are recoverable
+// Errors are NOT recoverable
 class MyException : Exception {
   // declare a constructor
+  // __FILE__ and __LINE__ are determined when the exception is called
+  // 'Throwable next' is useful if an exception handler throws an exception
   this(string message, string file = __FILE__, size_t line = __LINE__, Throwable next = null) {
     // parent class's constructor - Exception in this case
     super(message, file, line, next);
@@ -11,6 +15,7 @@ void main() {
   try
     throw new MyException("Message Here!");
   // prints a stacktrace too
+  // dmd -g <filename> @ compile time will include debug symbols
   catch(MyException e)
     writeln("caught ", e);
 }
